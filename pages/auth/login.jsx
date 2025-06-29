c50e504903647ddee6d55b8f3e8a8a78182e9316
cybev1 <cybev.io@gmail.com>
2025-06-29 07:59:37 +0000

    Add files via upload

---
+++ pages/auth/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+    </div>
+  );
+}
